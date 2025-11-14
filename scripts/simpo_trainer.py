4165d42b004d4abcca8f3eb0dfa408fc4ecfd40d
huangxu <xhuang441@qq.com>
2025-11-14 21:43:11 +0800

    train iter1

--- scripts/simpo_trainer.py
+++ scripts/simpo_trainer.py
@@ -14,13 +14,12 @@ import torch.nn.functional as F
 from accelerate import PartialState
 from datasets import Dataset
 from torch.utils.data import DataLoader
-from transformers import AutoModelForCausalLM, DataCollator, PreTrainedModel, PreTrainedTokenizerBase, Trainer
+from transformers import AutoModelForCausalLM, DataCollator, PreTrainedModel, PreTrainedTokenizerBase, Trainer, is_wandb_available
 from trl.trainer import CPOTrainer
 from transformers.trainer_callback import TrainerCallback
 from transformers.trainer_utils import EvalLoopOutput
-from transformers.utils import is_torch_fx_proxy
+from transformers.utils import is_torch_fx_proxy, is_peft_available
 
-from trl.import_utils import is_peft_available, is_wandb_available
 from scripts.simpo_config import SimPOConfig
 
 from dataclasses import dataclass
