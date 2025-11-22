d227904750ff3cbeb1f8b0bf3b63c8ab952e3f92
huangxu <xhuang441@qq.com>
2025-11-22 21:31:07 +0800

    train

--- mnpo_scripts/train.sh
+++ mnpo_scripts/train.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 ACCELERATE_LOG_LEVEL=info /mnt/data/.cache/conda/envs/sim/bin/accelerate launch \
-    --config_file accelerate_configs/deepspeed_zero3.yaml \
+    --config_file /root/xu/MNPO/accelerate_configs/deepspeed_zero3.yaml \
     -m mnpo_scripts.run_mnpo \
     "/root/xu/MNPO/training_configs/gemma-2-9b-it-mnpo-iter2-armo-td3.yaml"
--- mnpo_scripts/train2.sh
+++ mnpo_scripts/train2.sh
@@ -1,5 +1,5 @@
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 ACCELERATE_LOG_LEVEL=info /mnt/data/.cache/conda/envs/sim/bin/accelerate launch \
-    --config_file accelerate_configs/deepspeed_zero3.yaml \
+    --config_file /root/xu/MNPO/accelerate_configs/deepspeed_zero3.yaml \
     -m mnpo_scripts.run_mnpo \
     "/root/xu/MNPO/training_configs/gemma-2-9b-it-mnpo-iter2-armo-td4.yaml"
