5c9761dada80c1d2e19479a42549ec9a9ae4791c
huangxu <xhuang441@qq.com>
2025-11-25 00:28:52 +0800

    train

---
+++ evalscope/mt_api.sh
@@ -0,0 +1,10 @@
+
+API_KEY=$OPENAI_API_KEY
+
+/hai/scratch/fangwu97/miniconda3/envs/mt/bin/python gen_api_answer.py \
+    --model allenai/olmo-2-0325-32b-instruct \
+    --bench-name mt_bench \
+    --parallel 12 \
+    --max-tokens 4096 \
+    --openai-api-base "https://openrouter.ai/api/v1" \
+    --openai-api-key "$API_KEY"
