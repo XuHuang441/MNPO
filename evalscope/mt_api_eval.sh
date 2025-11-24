bbc6f166cd94fa3a788620bfd88e3ad6bba402d9
huangxu <xhuang441@qq.com>
2025-11-25 00:47:14 +0800

    train

--- evalscope/mt_api_eval.sh
+++ evalscope/mt_api_eval.sh
@@ -4,6 +4,6 @@ WORKDIR="/hai/scratch/fangwu97/xu/FastChat/fastchat/llm_judge"
 cd "${WORKDIR}"
 
 /hai/scratch/fangwu97/miniconda3/envs/mt/bin/python gen_judgment.py \
-    --model-list allenai gemma-2-9b-it_mnpo_stage_2_armo_beta1_ratio0.8_eta0.005_weights0.75-0.25_3pl.jsonl gemma-2-9b-it_mnpo_stage_2_athene_beta1_ratio0.85_eta0.005_weights0.75-0.25.jsonl gemma-2-9b-it_mnpo_stage_2_skywork_beta3_ratio0.85_eta0.01_weights1-0_3pl_fixed.jsonl Llama-3.1-Tulu-3-70B-DPO.jsonl SmolLM3-3B.jsonl \
+    --model-list olmo-2-0325-32b-instruct gemma-2-9b-it_mnpo_stage_2_armo_beta1_ratio0.8_eta0.005_weights0.75-0.25_3pl.jsonl gemma-2-9b-it_mnpo_stage_2_athene_beta1_ratio0.85_eta0.005_weights0.75-0.25.jsonl gemma-2-9b-it_mnpo_stage_2_skywork_beta3_ratio0.85_eta0.01_weights1-0_3pl_fixed.jsonl Llama-3.1-Tulu-3-70B-DPO.jsonl SmolLM3-3B.jsonl \
     --parallel 12 \
     --judge-model gpt-5-mini
