400600341500b1f2ce08e0756adbe86a0f4f7cbb
huangxu <xhuang441@qq.com>
2025-11-25 10:09:55 +0800

    train

--- evalscope/bash_arena.sh
+++ evalscope/bash_arena.sh
@@ -1,3 +1,3 @@
 /hai/scratch/fangwu97/miniconda3/envs/evalscope/bin/python /hai/scratch/fangwu97/xu/MNPO/evalscope/run_arena_hard_task.py \
-  --model-name gemma-2-9b-it_mnpo_stage_2_armo_beta1_ratio0.8_eta0.005_weights0.75-0.25_3pl \
+  --model-name gemma-2-9b-it \
   --port 1233
